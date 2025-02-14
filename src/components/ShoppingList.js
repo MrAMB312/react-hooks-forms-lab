81a5f877eeb8f3844891ea8617926630e7fa8fac
Alex Blomarz <a.blomarz@gmail.com>
2025-02-14 10:53:06 -0600

    complete react-hooks-forms-lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,33 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchValue, setSearchValue] = useState("");
+  const [itemsList, setItemsList] = useState(items);
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleSearchChange(event) {
+    setSearchValue(event.target.value);
+  }
 
-    return item.category === selectedCategory;
+  function handleAddItem(newItem) {
+    setItemsList([...itemsList, newItem])
+  }
+
+  const itemsToDisplay = itemsList.filter((item) => {
+    const matchCategory = selectedCategory === "All" || item.category === selectedCategory;
+    const matchSearch = searchValue === "" || item.name.toLowerCase().includes(searchValue.toLowerCase());
+    return matchCategory && matchSearch;
   });
+
+
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={handleAddItem} />
+      <Filter search={searchValue} onSearchChange={handleSearchChange} onCategoryChange={handleCategoryChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
